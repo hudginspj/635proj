914c5c1772e0a40fbe6a7a92b92b439fa88e03a2
hudginspj <hudginspj@mymail.vcu.edu>
2018-04-03 16:37:58 -0400

    features and classes

--- python/xSequences.py
+++ python/xSequences.py
@@ -1,12 +1,21 @@
+import textwrap
 # print("hello")
 with open('../spec/sequences_training.txt') as seq_file:
     with open('strip_seqs.txt', 'w') as out_file:
-        for line in seq_file.readlines():
-            seq = line.split(',')[0]
-            print(line)
-            # print(seq)
-            # print("---")
-            out_file.write(seq + '\n')
+        with open('635.fasta', 'w') as fasta_file:
+            counter = 0
+            for line in seq_file.readlines():
+                if counter >= 999:
+                    break
+                seq = line.split(',')[0]
+                print(line)
+                # print(seq)
+                # print("---")
+                out_file.write(f"{seq}\n")
+                fasta_file.write(f">seq{counter}\n")
+                fasta_file.write(textwrap.fill(seq, width=100))
+                fasta_file.write("\n")
+                counter += 1
 
 
 
