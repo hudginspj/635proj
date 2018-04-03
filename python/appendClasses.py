6724f161a5abdba2630dc272f2c6d93e1d54e503
hudginspj <hudginspj@mymail.vcu.edu>
2018-04-03 17:15:56 -0400

    improved appending of predictions

--- python/appendClasses.py
+++ python/appendClasses.py
@@ -1,17 +1,20 @@
-import textwrap
-# print("hello")
 FEATURES_FILENAME = 'seq-34702.out'
+
+def ignoreFirst(commaSeperatedLine):
+    return ','.join(commaSeperatedLine.split(',')[1:])
+
 with open('../spec/sequences_training.txt') as original:
     with open('../features/' + FEATURES_FILENAME) as features:
-        with open('../features/classes-' + FEATURES_FILENAME, 'w') as out_file:
+        with open('../features/pred-' + FEATURES_FILENAME, 'w') as out_file:
             original_lines = original.readlines()
             features_lines = features.readlines()
-            out_file.write(features_lines[0] + ", class\n")
+            out_file.write(ignoreFirst(features_lines[0].strip()) + ",class\n")
             for i in range(1, len(features_lines)):
-                prot_class = original_lines[i].split(',')[1]
-                feats = features_lines[i]
-                out_file.write(feats + ", " + prot_class + "\n")
-                
+                prot_class = original_lines[i].strip().split(',')[1]
+                feats = features_lines[i].strip()
+                feats = ignoreFirst(feats) #Uncomment this line to ignore the first "feature" (seqn)
+                out_file.write(feats + ', ' + prot_class + '\n')
+
 
 
 
