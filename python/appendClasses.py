f789dd27db3ff5b3d19b9f94246511aea4d3b3d8
DESKTOP-SAJTGB6\Fatima <fmasche@gmail.com>
2018-04-13 16:04:02 -0400

    Adding combinePredictionFiles.py to combine to a unique file all the predictions done with ProtrWeb. appendClasses.py was modified to support files when the sequences of the prediction file doesn't have the same index as the original data set.

--- python/appendClasses.py
+++ python/appendClasses.py
@@ -1,22 +1,30 @@
-FEATURES_FILENAME = 'nikki_features.csv'
+#FEATURES_FILENAME = 'nikki_features.csv'
+FEATURES_FILENAME = 'fa_feat_ProtrWeb.csv'
+DIFF_IN_INDEX = True  #To maintain the same class, True if the prediction file doesn't have the same indexes as the original data set.
 
 def ignoreFirst(commaSeperatedLine):
     return ','.join(commaSeperatedLine.split(',')[1:])
 
-with open('../spec/sequences_training.txt') as original:
-    with open('../features/' + FEATURES_FILENAME) as features:
-        with open('../features/pred-' + FEATURES_FILENAME, 'w') as out_file:
+with open('./spec/sequences_training.txt') as original:
+    with open('./features/' + FEATURES_FILENAME) as features:
+        with open('./features/pred-' + FEATURES_FILENAME, 'w') as out_file:
             original_lines = original.readlines()
             features_lines = features.readlines()
             out_file.write(ignoreFirst(features_lines[0].strip()) + ",class\n")
             # print(len(features_lines), len(original_lines))
             for i in range(1, len(features_lines)):
                 # print(i, original_lines[i].__len__())
-                prot_class = original_lines[i-1].strip().split(',')[1]
+
+                if (DIFF_IN_INDEX):
+                    feat_seq_number = features_lines[i].strip().split(',')[0].replace('"', '')
+                    feat_seq_number = feat_seq_number[3:]               
+                    prot_class = original_lines[int(feat_seq_number)-1].strip().split(',')[1]
+                else:
+                    prot_class = original_lines[i-1].strip().split(',')[1]
+                
                 feats = features_lines[i].strip()
                 feats = ignoreFirst(feats) #Uncomment this line to ignore the first "feature" (seqn)
                 out_file.write(feats + ', ' + prot_class + '\n')
-
 
 
 
---
+++ python/combinePredictionFiles.py
@@ -0,0 +1,27 @@
+from os import listdir, path
+import json
+import csv
+
+files_path = "./features/fa"
+files = listdir(files_path)
+files.sort()
+files.remove('test')
+#print(files)
+
+with open('./features/fa_feat_ProtrWeb.csv', 'w') as out_file:
+    for i, fileName in enumerate(files):python python/
+
+        tk_path = path.join(files_path, fileName)
+        print("Loading %s " % tk_path)
+    
+        with open(tk_path, 'r', encoding='utf-8') as features:        
+            features_lines = features.readlines()
+            if (i == 0):  # We only print the header with the features when reading the first file.
+                out_file.write(','.join(features_lines[0].strip().split(',')[0:]))
+                out_file.write('\n')
+            
+            for j in range(1, len(features_lines)):
+                feats = features_lines[j].strip()
+                feats = ','.join(feats.split(',')[0:])
+                
+                out_file.write(feats + '\n')
