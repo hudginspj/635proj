fcbdab40532ba8211b20140d445650d7a33ffc35
hudginspj <hudginspj@mymail.vcu.edu>
2018-04-28 19:15:36 -0400

    final_data_generation

--- feature_generation/final_data_generation.py
+++ feature_generation/final_data_generation.py
@@ -1,41 +1,68 @@
+from os import listdir, path
+import csv
+
+TRAIN_OR_TEST = "training"
 
 
-def gen_data_training():
-    pass
+def gen_data():
+    classes = read_classes()
+    fatima_features, fatima_rows = read_features(f"./features/{TRAIN_OR_TEST}_fatima")
+    paul_features, paul_rows = read_features(f"./features/{TRAIN_OR_TEST}_paul")
+    with open(f"./training_data/{TRAIN_OR_TEST}_all.csv", 'w') as out_file:
+        out_file.write(','.join(fatima_features))
+        out_file.write(',')
+        out_file.write(','.join(paul_features))
+        out_file.write(',class\n')
+        for seq in fatima_rows.keys():
+            out_file.write(','.join(fatima_rows[seq]))
+            out_file.write(',')
+            try:
+                paul_row = ','.join(paul_rows[seq])
+            except:
+                print("couldn't load row for ", seq)
+            out_file.write(paul_row)
+            out_file.write(f',{classes[seq]}\n')
+            
+
+def read_classes():
+    classes = {}
+    with open(f'./spec/sequences_{TRAIN_OR_TEST}.txt') as original:
+        original_lines = original.readlines()
+        for i in range(len(original_lines)):
+            classes[f'seq{i}'] = original_lines[i].split(',')[-1].strip()
+    return classes
 
 
-def gen_data_test():
-    pass
+def read_features(files_path):
+    feature_names = []
+    rows = {}
 
-
-def gen_data(out_file_name, ):
-    pass
-
-
-def fatima_x():
-    files_path = "./features/fa"
     files = listdir(files_path)
     files.sort()
     # files.remove('test')
     #print(files)
 
-    with open('./features/fa_feat_ProtrWeb.csv', 'w') as out_file:
-        for i, fileName in enumerate(files):
+    for i, fileName in enumerate(files):
 
-            tk_path = path.join(files_path, fileName)
-            print("Loading %s " % tk_path)
-        
-            with open(tk_path, 'r', encoding='utf-8') as features:        
-                features_lines = features.readlines()
-                if (i == 0):  # We only print the header with the features when reading the first file.
-                    out_file.write(','.join(features_lines[0].strip().split(',')[0:]))
-                    out_file.write('\n')
+        tk_path = path.join(files_path, fileName)
+        print("Loading %s " % tk_path)
+    
+        with open(tk_path, 'r', encoding='utf-8') as features:        
+            features_lines = features.readlines()
+            if (i == 0):  # We only print the header with the features when reading the first file.
+                feature_names = features_lines[0].strip().split(',')[1:]
+                #print(feature_names)
+            
+            for j in range(1, len(features_lines)):
+                feats = features_lines[j].strip()
+                feats = feats.split(',')
+                rows[feats[0].strip('"')] = feats[1:]
+
+                #print(feats[0:2])
+    return feature_names, rows
                 
-                for j in range(1, len(features_lines)):
-                    feats = features_lines[j].strip()
-                    feats = ','.join(feats.split(',')[0:])
-                    
-                    out_file.write(feats + '\n')
 
 if __name__ == "__main__":
-    gen_data_training()
+    #read_features("./features/fa")
+    gen_data()
+    #read_features(f"./features/{TRAIN_OR_TEST}_paul")
